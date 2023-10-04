33c44da0ca8a43059ed309e76706bff960f04597
ollie <67156692+0xEillo@users.noreply.github.com>
2023-10-04 09:32:51 +0100

    feat: add remappings.txt

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,88 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+import {IHooks} from "v4-core/interfaces/IHooks.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract BinanceAttestationHook is IHooks {
+    error HookNotImplemented();
+
+    function beforeInitialize(
+        address sender,
+        PoolKey calldata key,
+        uint160 sqrtPriceX96,
+        bytes calldata hookData
+    ) external returns (bytes4) {
+        revert HookNotImplemented();
     }
 
-    function increment() public {
-        number++;
+    function afterInitialize(
+        address sender,
+        PoolKey calldata key,
+        uint160 sqrtPriceX96,
+        int24 tick,
+        bytes calldata hookData
+    ) external returns (bytes4) {
+        revert HookNotImplemented();
+    }
+
+    function beforeModifyPosition(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.ModifyPositionParams calldata params,
+        bytes calldata hookData
+    ) external returns (bytes4) {
+        revert HookNotImplemented();
+    }
+
+    function afterModifyPosition(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.ModifyPositionParams calldata params,
+        BalanceDelta delta,
+        bytes calldata hookData
+    ) external returns (bytes4) {
+        revert HookNotImplemented();
+    }
+
+    function beforeSwap(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.SwapParams calldata params,
+        bytes calldata hookData
+    ) external returns (bytes4) {
+        // TODO: implement this function
+    }
+
+    function afterSwap(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.SwapParams calldata params,
+        BalanceDelta delta,
+        bytes calldata hookData
+    ) external returns (bytes4) {
+        revert HookNotImplemented();
+    }
+
+    function beforeDonate(
+        address sender,
+        PoolKey calldata key,
+        uint256 amount0,
+        uint256 amount1,
+        bytes calldata hookData
+    ) external returns (bytes4) {
+        revert HookNotImplemented();
+    }
+
+    function afterDonate(
+        address sender,
+        PoolKey calldata key,
+        uint256 amount0,
+        uint256 amount1,
+        bytes calldata hookData
+    ) external returns (bytes4) {
+        revert HookNotImplemented();
     }
 }
