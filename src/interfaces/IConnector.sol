fea1ffb3ffa9f124a445f39c72a988042c81ec92
ollie <67156692+0xEillo@users.noreply.github.com>
2023-11-28 11:32:45 +0000

    chore

--- src/interfaces/IConnector.sol
+++ src/interfaces/IConnector.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
-import {Attestation as EASAttestation} from "@ethereum-attestation-service/eas-contracts/contracts/IEAS.sol";
-import {Attestation as VeraxAttestation} from "linea-attestation-registry/AttestationRegistry.sol";
+import {Attestation as EASAttestation} from "eas/IEAS.sol";
+import {Attestation as VeraxAttestation} from "lar/AttestationRegistry.sol";
 
 /**
  * @title IConnector
